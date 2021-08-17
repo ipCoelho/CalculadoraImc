b7f1863635a39491d02529486824ed38defa84ca
ISRAEL PABLO COELHO <21193386@CFP127ALUNO.LOCAL>
2021-08-17 16:58:10 -0300

    Atualizados

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // variáveis
-const nome = document.getElementById("nome")
+const valueNome = document.getElementById("valueNome")
 const peso = document.getElementById("valuePeso")
 const altura = document.getElementById("valueAltura")
 let resultado = document.getElementById("resultado")
@@ -9,11 +9,36 @@ const btnCalcular = document.getElementById("botao")
 
 //funções
 function calcularImc() {
-    imc = peso.value / altura.value ** 2
-    resultado.textContent = "O seu imc é: "+imc
+    let imc = peso.value / altura.value ** 2
+    imc.fixedTo(2)
+    nome = valueNome.value
+    if (imc < 18.5) {
+        resultado.textContent = nome+", seu imc é "+imc+". "+"Você está abaixo do peso."
+    } 
+    else {
+        if (imc >= 18.5 && imc <= 24.9) {
+            resultado.textContent = nome+", seu imc é "+imc+". "+"Parabéns! Você está no peso ideal."
+        } else {
+            if (imc >= 25 && imc <= 29.9) {
+                resultado.textContent = nome+", seu imc é "+imc+". "+"Você está acima do peso."
+            } 
+            else {
+                if (imc >= 30 && imc <= 34.9) {
+                    resultado.textContent = nome+", seu imc é "+imc+". "+"Cuidado. Você está no grau I de obesidade."
+                } 
+                else {
+                    if (imc >= 35 && imc <= 39.9)  {
+                        resultado.textContent = nome+", seu imc é "+imc+". "+"Cuidado. Você está no grau II de obesidade."
+                    } 
+                    else {
+                            resultado.textContent = nome+", seu imc é "+imc+". "+"Cuidado. Você está no grau III de obesidade."
+                    }
+                }
+            }
+        }
+    
+    }
 }
 
 //botao-evento
 btnCalcular.addEventListener("click", calcularImc)
-
-console.log()
