aef0f78dea7aa02b3fd13ebb9c0466ee04f0f57e
ISRAEL PABLO COELHO <21193386@CFP127ALUNO.LOCAL>
2021-08-24 16:56:10 -0300

    'index.html' finalizado; 'professor.js' finalizado;

--- professor.js
+++ professor.js
@@ -1,5 +1,5 @@
 // JavaScript~correction 'Calculadora IMC' 24/08/2021
-
+// Expressões Regulares
 // Functions
 function toCalculateImc(weight, height){
     return weight / (height ** 2)
@@ -10,7 +10,7 @@ function toClassificateImc(imc){
     if (imc < 18.5){
         category = 'abaixo do peso'
     } else if (imc < 25) {
-        category = 'com peso ideal. Parabéns!'
+        category = "com peso ideal. <span class='azul'>Parabéns!</span>"
     } else if (imc < 30) {
         category = 'levemente acima do peso.'
     } else if (imc < 35) {
@@ -18,7 +18,7 @@ function toClassificateImc(imc){
     } else if (imc < 40) {
         category = 'com obesidade grau II'
     } else {
-        category = 'com obesidade grau III'
+        category = "com obesidade grau III' <span class='vermelho'>Cuidado!</span>"
     }
     return category
 }
@@ -34,19 +34,26 @@ function showResult() {
     const weight = document.getElementById('iptWeight').value.replace("," , ".")
     const height = document.getElementById('iptHeight').value.replace("," , ".")
     
-    if (camposValidos()) {
+    if (toValidateFields()) {
         // Saving the functions into variavables
         const imc = toCalculateImc(weight, height)
         const classification = toClassificateImc(imc)
         // Constructing the Output Message into the div's textContent
-        result.textContent = `${name} seu IMC é ${imc.toFixed(1)} e você está ${classification}` // toFixed() -> Method to fix the decimal points.
+        result.innerHTML = `${name} seu IMC é ${imc.toFixed(1)} e você está ${classification}` // toFixed() -> Method to fix the decimal points.
     } else {
         result.textContent = `Preencha todos os campos!`
     }
 }
 
+function calculateWithEnter(evento) {
+    if (evento.key === "Enter") {
+        showResult()
+    }
+}
+
 // Events
 document.getElementById('button').addEventListener("click", showResult)
+document.querySelector('form').addEventListener("keypress", calculateWithEnter)
 
 
 
